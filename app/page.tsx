af6ff807236c19da322a76ba058dc8132a29b592
Willi#m ⬣ <email@williamli.dev>
2023-12-20 20:00:42 +0800

    fix page error

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      All the world's a stage
+      All the world is a stage.
     </main>
   );
 }
